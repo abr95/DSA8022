6ebfde12b81a867d9919cceeb5079a8dc1ac006d
Andrew Brown <40351876@eeecs.qub.ac.uk>
2022-03-01 14:11:56 +0000

    App with potential issue - cannot reuse outputs

---
+++ Rmds/App3/app.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(shiny)
+
+options(shiny.host = '0.0.0.0')
+options(shiny.port = 8888)
+
+ui <- fluidPage(
+  textInput(inputId = "name", label = "Name", value = "AB"),
+  textOutput(outputId = "name"),
+  textOutput(outputId = "name2"),
+)
+
+server <- function(
+  input, 
+  output
+) {
+  output$name <- renderText({
+    paste("Big Bad ", input$name)
+  })
+  output$name2 <- renderText({
+    paste("Big Bad ", input$name)
+  })
+}
+
+shinyApp(
+  ui = ui, 
+  server = server
+)
