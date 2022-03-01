3d91d78d9043c0db067d678e4dc260b5b532a0b3
Andrew Brown <40351876@eeecs.qub.ac.uk>
2022-03-01 14:28:13 +0000

    Updated with MVP

--- Rmds/App3/app.R
+++ Rmds/App3/app.R
@@ -7,7 +7,7 @@ options(shiny.port = 8888)
 ui <- fluidPage(
   textInput(inputId = "name", label = "Name", value = "AB"),
   textOutput(outputId = "name"),
-  textOutput(outputId = "name2"),
+  textOutput(outputId = "name2")
 )
 
 server <- function(
@@ -15,10 +15,10 @@ server <- function(
   output
 ) {
   output$name <- renderText({
-    paste("Big Bad ", input$name)
+    input$name
   })
   output$name2 <- renderText({
-    paste("Big Bad ", input$name)
+    input$name
   })
 }
 
